7d532a6cd7413e15fe84fe9c1cb8837acd375076
abhi-jha1 <63658450+abhi-jha1@users.noreply.github.com>
2020-07-25 03:00:52 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+class App extends React.Component {
+  constructor() {
+    super();
+    this._handleKeyPress = this._handleKeyPress.bind(this);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      value1: '',
+      value2: '',
+      value3: '',
+      value4: '',
+      value5: '',
+      value6: ''
+
+    }
+
+
+  }
+
+  componentDidMount() {
+    for (let x in this.refs) {
+      this.refs[x].onkeyup = (e) =>
+        this._handleKeyPress(e, this.refs[x]);
+
+    }
+    this.refs.input1.focus();
+  }
+
+
+
+
+
+  _handleKeyPress(e, field) {
+
+    let maxLength = e.target.maxLength;
+    let inputLength = e.target.value.length;
+    if (inputLength >= maxLength) {
+
+      let next = this.refs[field.name].nextSibling;
+      if (next && next.tagName === "INPUT") {
+        next.focus();
+      }
+    }
+    else if (inputLength === 0) {
+
+      let previous = this.refs[field.name].previousSibling;
+      if (previous && previous.tagName === "INPUT") {
+        previous.focus();
+      }
+    }
+  }
+
+  validate1 = (e) => {
+    const value = (e.target.validity.valid) ? e.target.value : this.state.value1;
+
+    this.setState({ value1: value });
+  }
+  validate2 = (e) => {
+    const value = (e.target.validity.valid) ? e.target.value : this.state.value2;
+
+    this.setState({ value2: value });
+  }
+  validate3 = (e) => {
+    const value = (e.target.validity.valid) ? e.target.value : this.state.value3;
+
+    this.setState({ value3: value });
+  }
+  validate4 = (e) => {
+    const value = (e.target.validity.valid) ? e.target.value : this.state.value4;
+
+    this.setState({ value4: value });
+  }
+  validate5 = (e) => {
+    const value = (e.target.validity.valid) ? e.target.value : this.state.value5;
+
+    this.setState({ value5: value });
+  }
+  validate6 = (e) => {
+    const value = (e.target.validity.valid) ? e.target.value : this.state.value6;
+
+    this.setState({ value6: value });
+  }
+
+
+
+
+  render() {
+    return (
+      <div className='credit-card'>
+
+        <form style={{ textAlign: "center" }}>
+          <h3>Card Number*</h3>
+          <input className='input' type="text" pattern="[0-9]*" name="input1" ref='input1' maxLength="4" onInput={this.validate1} value={this.state.value1} placeHolder='****' />
+          <input className='input' type="text" pattern="[0-9]*" name="input2" ref='input2' maxLength="4" onInput={this.validate2} value={this.state.value2} placeHolder='****' />
+          <input className='input' type="text" pattern="[0-9]*" name="input3" ref='input3' maxLength="4" onInput={this.validate3} value={this.state.value3} placeHolder='****' />
+          <input className='input' type="text" pattern="[0-9]*" name="input4" ref='input4' maxLength="4" onInput={this.validate4} value={this.state.value4} placeHolder='****' />
+          <input className="input1" type="text" pattern="[0-9]*" placeHolder="MM / YY" maxLength='4' onInput={this.validate5} value={this.state.value5} />
+          <input className="input" type="text" pattern="[0-9]*" placeHolder='cvv' pattern="[0-9]*" onInput={this.validate6} value={this.state.value6} maxLength='3' />
+
+
+
+
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
