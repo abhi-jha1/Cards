eed41ddfc0910f61a65db19e44e53cae39c01026
abhi-jha1 <63658450+abhi-jha1@users.noreply.github.com>
2020-08-04 20:28:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -70,9 +70,11 @@ class App extends React.Component {
     this.setState({ value4: value });
   }
   validate5 = (e) => {
+
     const value = (e.target.validity.valid) ? e.target.value : this.state.value5;
 
     this.setState({ value5: value });
+
   }
   validate6 = (e) => {
     const value = (e.target.validity.valid) ? e.target.value : this.state.value6;
@@ -94,7 +96,7 @@ class App extends React.Component {
           <input className='input' type="text" pattern="[0-9]*" name="input3" ref='input3' maxLength="4" onInput={this.validate3} value={this.state.value3} placeHolder='****' />
           <input className='input' type="text" pattern="[0-9]*" name="input4" ref='input4' maxLength="4" onInput={this.validate4} value={this.state.value4} placeHolder='****' />
           <br /> <input className="input1" type="text" pattern="[0-9]*" name="input5" ref="input5" placeHolder="MM / YY" maxLength='4' onInput={this.validate5} value={this.state.value5} />
-          <input className="input" type="text" pattern="[0-9]*" name="input6" ref="input6" placeHolder='cvv' pattern="[0-9]*" onInput={this.validate6} value={this.state.value6} maxLength='3' />
+          <input className="input" type="password" pattern="[0-9]*" name="input6" ref="input6" placeHolder='cvv' pattern="[0-9]*" onInput={this.validate6} value={this.state.value6} maxLength='3' />
 
 
 
