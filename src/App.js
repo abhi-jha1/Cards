cfd26e39b30819af4c419c8dc1035da20b345bd8
abhi-jha1 <63658450+abhi-jha1@users.noreply.github.com>
2020-08-04 20:31:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -91,11 +91,11 @@ class App extends React.Component {
 
         <form style={{ textAlign: "center" }}>
           <h3>Card Number*</h3>
-          <input className='input' type="text" pattern="[0-9]*" name="input1" ref='input1' maxLength="4" onInput={this.validate1} value={this.state.value1} placeHolder='****' />
-          <input className='input' type="text" pattern="[0-9]*" name="input2" ref='input2' maxLength="4" onInput={this.validate2} value={this.state.value2} placeHolder='****' />
-          <input className='input' type="text" pattern="[0-9]*" name="input3" ref='input3' maxLength="4" onInput={this.validate3} value={this.state.value3} placeHolder='****' />
-          <input className='input' type="text" pattern="[0-9]*" name="input4" ref='input4' maxLength="4" onInput={this.validate4} value={this.state.value4} placeHolder='****' />
-          <br /> <input className="input1" type="text" pattern="[0-9]*" name="input5" ref="input5" placeHolder="MM / YY" maxLength='4' onInput={this.validate5} value={this.state.value5} />
+          <input className='input' type="number" pattern="[0-9]*" name="input1" ref='input1' maxLength="4" onInput={this.validate1} value={this.state.value1} placeHolder='****' />
+          <input className='input' type="number" pattern="[0-9]*" name="input2" ref='input2' maxLength="4" onInput={this.validate2} value={this.state.value2} placeHolder='****' />
+          <input className='input' type="number" pattern="[0-9]*" name="input3" ref='input3' maxLength="4" onInput={this.validate3} value={this.state.value3} placeHolder='****' />
+          <input className='input' type="number" pattern="[0-9]*" name="input4" ref='input4' maxLength="4" onInput={this.validate4} value={this.state.value4} placeHolder='****' />
+          <br /> <input className="input1" type="number" pattern="[0-9]*" name="input5" ref="input5" placeHolder="MM / YY" maxLength='4' onInput={this.validate5} value={this.state.value5} />
           <input className="input" type="password" pattern="[0-9]*" name="input6" ref="input6" placeHolder='cvv' pattern="[0-9]*" onInput={this.validate6} value={this.state.value6} maxLength='3' />
 
 
